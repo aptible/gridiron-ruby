7da7fdbf84f28b1af2cc49b8d43148e94be88d58
Frank Macreery <frank@macreery.com>
2014-07-23 21:56:44 -0700

    Status is now determined server-side

--- lib/aptible/gridiron/criterion.rb
+++ lib/aptible/gridiron/criterion.rb
@@ -10,27 +10,8 @@ module Aptible
       field :description
       field :evidence_type
       field :scope
+      field :status
       field :default_expiry
-
-      def documents
-        evidence_type == 'document' ? super : []
-      end
-
-      def events
-        evidence_type == 'event' ? super : []
-      end
-
-      def status
-        green? ? 'green' : 'red'
-      end
-
-      # TODO: Move to Gridiron, or fix logic
-      def green?
-        return true if evidence_type == 'event'
-
-        return false unless documents.first
-        documents.first.expires_at > Time.now
-      end
     end
   end
 end
--- lib/aptible/gridiron/version.rb
+++ lib/aptible/gridiron/version.rb
@@ -1,5 +1,5 @@
 module Aptible
   module Gridiron
-    VERSION = '0.2.0'
+    VERSION = '0.2.1'
   end
 end
